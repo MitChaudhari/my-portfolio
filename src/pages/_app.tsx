db9cb8d4bed1325f1b23a3e06851366814a35fdd
Mitansh Chaudhari <mitansh807@gmail.com>
2024-08-31 19:57:38 -0500

    Added the Analytics component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'; // Import global styles
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react'; // Import the Analytics component
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
